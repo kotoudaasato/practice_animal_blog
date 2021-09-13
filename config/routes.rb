d8d173b77cddc24e387e38f3c35cac74b3426777
kotoudaasato <aru.cam.wanwan@gmail.com>
2021-09-13 10:16:45 +0000

    deploy setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+
+  scope module: :public do
+    root to: 'homes#top'
+    devise_for :users
+      resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
