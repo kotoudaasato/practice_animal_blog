10017991bde6ebae79b7864f23ea79ccc7525efd
kotoudaasato <aru.cam.wanwan@gmail.com>
2021-09-19 04:44:28 +0000

    [update]post function

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -10,8 +10,28 @@ class Public::PostsController < ApplicationController
     redirect_to post_path(@post.id)
   end
 
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
   private
