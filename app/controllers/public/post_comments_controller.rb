91e585ee5aa275f1bea1b16a6f430f5916b15af8
kotoudaasato <aru.cam.wanwan@gmail.com>
2021-09-21 16:07:26 +0000

    [Update]comment_function

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,15 +1,16 @@
 class Public::PostCommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to request.referer
+    @post = Post.find(params[:post_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    @comment.save
+    render :index
   end
 
   def destroy
      PostComment.find_by(id:params[:id]).destroy
-     redirect_to request.referer
+     @post = Post.find(params[:post_id])
+     render :index
   end
 
   private
